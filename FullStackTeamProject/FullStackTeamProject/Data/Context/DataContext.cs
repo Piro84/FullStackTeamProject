968e6f5f745152edc84c5d293aa69b968dffadd0
Unknown <19vign@elmbrookstudents.org>
2019-06-25 14:34:36 -0500

    init

---
+++ FullStackTeamProject/FullStackTeamProject/Data/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using FullStackTeamProject.Data;
+
+namespace FullStackTeamProject.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
